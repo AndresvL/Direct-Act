13db2ddfc9c49ef3ef08175a8c4e84eb25523676
Andres van Lummel <andresvanlummel@hotmail.com>
2015-05-01 11:17:36 +0200

    superklasse

--- src/domein/Gebruiker.java
+++ src/domein/Gebruiker.java
@@ -1,16 +1,25 @@
 package domein;
 
 public abstract class Gebruiker {
-	protected String username;
+	protected String email, wachtwoord, voornaam, achternaam;
 	
-	public Gebruiker(String uName){
-		username = uName;
+	public Gebruiker(String eM, String ww, String vN, String aN){
+		email = eM;
+		wachtwoord = ww;
+		voornaam = vN;
+		achternaam = aN;
 	}
-	protected void setUsername(String uName){
-		username = uName;
+	protected void setEmail(String em){
+		email = em;
 	}
-	protected String getUsername(){
-		return username;
+	protected void setWachtwoord(String ww){
+		wachtwoord = ww;
+	}
+	protected void setVoornaam(String vn){
+		voornaam = vn;
+	}
+	protected void setAchternaam(String an){
+		achternaam = an;
 	}
 	
 }
