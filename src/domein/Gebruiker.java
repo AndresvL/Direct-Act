e6c8242f33b899246d958772e7867230b3909cfb
Denniemc <denniemc@hotmail.com>
2015-05-01 10:33:49 +0200

    ignore

--- src/domein/Gebruiker.java
+++ src/domein/Gebruiker.java
@@ -1,5 +1,16 @@
 package domein;
 
 public abstract class Gebruiker {
-
+	protected String username;
+	
+	public Gebruiker(String uName){
+		username = uName;
+	}
+	protected void setUsername(String uName){
+		username = uName;
+	}
+	protected String getUsername(){
+		return username;
+	}
+	
 }
