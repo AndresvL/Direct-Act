169c646497fef6f1f5618f840e84b29690fc32c2
Andres van Lummel <andresvanlummel@hotmail.com>
2015-05-22 17:34:40 +0200

    tijdPerVraag gefixt

--- src/servlets/LoginStudentServlet.java
+++ src/servlets/LoginStudentServlet.java
@@ -1,7 +1,9 @@
 package servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,44 +16,75 @@ import controller.StudentController;
 import domein.Student;
 import domein.Vraag;
 
-public class LoginStudentServlet extends HttpServlet{
+public class LoginStudentServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 194503685680L;
 	private StudentController controller = new StudentController();
 	private VraagController vc = new VraagController();
-	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	private Calendar cal = Calendar.getInstance();
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		Student s = new Student();
 		Vraag v = vc.eersteVraag();
-		if(v.getAfbeelding() == null){
+		if (v.getAfbeelding() == null) {
 			String blob = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAANSURBVBhXY/j//z8DAAj8Av6IXwbgAAAAAElFTkSuQmCC";
 			v.setAfbeelding(blob);
 		}
-		
+
 		RequestDispatcher rd = null;
 		int code = Integer.parseInt(req.getParameter("code"));
-		if(req.getParameter("button").equals("login")){
-			if(controller.checkCode(code)){
-				s = controller.setStudent(s,code);
+		if (req.getParameter("button").equals("login")) {
+			if (controller.checkCode(code)) {
+				s = controller.setStudent(s, code);
 				req.getSession().setAttribute("student", s);
 				req.getSession().setAttribute("vraag", v.getVraagstelling());
 				req.getSession().setAttribute("vraagnummer", v.getNummer());
 				req.getSession().setAttribute("context", v.getContext());
 				req.getSession().setAttribute("type", v.getType());
 				req.getSession().setAttribute("plaatje", v.getAfbeelding());
-				req.getSession().setAttribute("rekenmachine", v.isRekenmachine());
-				req.getSession().setAttribute("minuten", Calendar.MINUTE);
-				req.getSession().setAttribute("seconden", Calendar.SECOND);
-			req.getSession().setAttribute("toetsnummer", vc.getVolgendToetsNummer(true,s.getCode()));
-				/*if(s.isFirstTime())rd = req.getRequestDispatcher("/enquete.jsp");
-				else */rd=req.getRequestDispatcher("/toets-vraag.jsp");
-								
-			}else{
+				req.getSession().setAttribute("rekenmachine",v.isRekenmachine());
+				req.getSession().setAttribute("tijd",getUur(System.currentTimeMillis()));
+				System.out.println("getToday(long timemilles) "+ getUur(System.currentTimeMillis()));
+				req.getSession().setAttribute("uren",getUur(System.currentTimeMillis()));
+				req.getSession().setAttribute("minuten",getMinuut(System.currentTimeMillis()));
+				req.getSession().setAttribute("seconden",getSeconde(System.currentTimeMillis()));
+				req.getSession().setAttribute("toetsnummer",vc.getVolgendToetsNummer(true, s.getCode()));
+				if (s.isFirstTime()) {
+					rd = req.getRequestDispatcher("/enquete.jsp");
+				} else {
+					rd = req.getRequestDispatcher("/toets-vraag.jsp");
+				}
+			} else {
 				req.setAttribute("msgs", "code bestaat niet");
-			rd = req.getRequestDispatcher("/login-student.jsp");
+				rd = req.getRequestDispatcher("/login-student.jsp");
 			}
 		}
 		rd.forward(req, resp);
-		
+
+	}
+
+	public int getUur(long millisecs) {
+		cal = Calendar.getInstance();
+		SimpleDateFormat uur = new SimpleDateFormat("HH");
+		Date resultdate = new Date(millisecs);
+		int HH = Integer.parseInt(uur.format(resultdate));
+		return HH;
+	}
+
+	public int getMinuut(long millisecs) {
+		cal = Calendar.getInstance();
+		SimpleDateFormat minuut = new SimpleDateFormat("mm");
+		Date resultdate = new Date(millisecs);
+		int mm = Integer.parseInt(minuut.format(resultdate));
+		return mm;
+	}
+
+	public int getSeconde(long millisecs) {
+		cal = Calendar.getInstance();
+		SimpleDateFormat seconde = new SimpleDateFormat("ss");
+		Date resultdate = new Date(millisecs);
+		int ss = Integer.parseInt(seconde.format(resultdate));
+		return ss;
 	}
 }
--- src/servlets/ToetsServlet.java
+++ src/servlets/ToetsServlet.java
@@ -1,13 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.AntwoordController;
 import controller.VraagController;
@@ -22,6 +25,7 @@ public class ToetsServlet extends HttpServlet{
 	private static final long serialVersionUID = 1120482309534L;
 	private AntwoordController antw = new AntwoordController();
 	private VraagController vrg = new VraagController();
+	private Calendar cal = Calendar.getInstance();	
 	
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
@@ -29,20 +33,20 @@ public class ToetsServlet extends HttpServlet{
 		if(req.getParameter("button").equals("volgende")){
 			Antwoord a = new Antwoord();
 			int nrs = (Integer)req.getSession().getAttribute("vraagnummer");
-			Vraag huidig = new Vraag(nrs, (String)req.getSession().getAttribute("type"));
-			
+			Vraag huidig = new Vraag(nrs, (String)req.getSession().getAttribute("type"));			
 			int nr = huidig.getNummer();
-			int minuten = (Integer)req.getSession().getAttribute("minuten") - Calendar.MINUTE;
-			int seconds = (Integer)req.getSession().getAttribute("seconden") - Calendar.SECOND + (minuten * 60);
+			int uur = (Integer)req.getSession().getAttribute("uren");
+			int min = (Integer)req.getSession().getAttribute("minuten");
+			int sec = (Integer)req.getSession().getAttribute("seconden");
 			a.setNummer(nr);
-			a.setTijd(seconds);
+			a.setTijd(getVraagTijd(sec, min, uur, (System.currentTimeMillis())));
 			a.setAntwoord((String)req.getParameter("antwoord"));
 			a.setCategorie(huidig.getType());
 			a.setToetsNummer((Integer)req.getSession().getAttribute("toetsnummer"));
 			a.setVraagNummer(nr);
 			a.setHeeftRekenmachineGebruikt(false);
-			antw.verwerk(a);
-			if(vrg.isVolgendeVraag(nr + 1) != false){
+			antw.verwerk(a);		
+			if(vrg.isVolgendeVraag(nr + 1) != false){				
 				Vraag v = vrg.volgendeVraag(nr + 1);
 				if(v.getAfbeelding() == null){
 					String blob = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuNWWFMmUAAAANSURBVBhXY/j//z8DAAj8Av6IXwbgAAAAAElFTkSuQmCC";
@@ -54,14 +58,31 @@ public class ToetsServlet extends HttpServlet{
 				req.getSession().setAttribute("vraag", v.getVraagstelling());
 				req.getSession().setAttribute("plaatje", v.getAfbeelding());
 				req.getSession().setAttribute("rekenmachine", v.isRekenmachine());
-				req.getSession().setAttribute("minuten", Calendar.MINUTE);
-				req.getSession().setAttribute("seconden", Calendar.SECOND);
-				System.out.println(req.getSession().getAttribute("toetsnummer"));
 				rd = req.getRequestDispatcher("/toets-vraag.jsp");
 			}else rd = req.getRequestDispatcher("/toets-eind.jsp");
 		}
 		rd.forward(req,resp);
 	}
-	
-	
+
+	public Date getToday(){
+        cal = Calendar.getInstance();        
+        return cal.getTime();
+    }
+	private int getVraagTijd(int sec, int min, int ur, long millisecs) {
+		Date resultdate = new Date(millisecs);
+//		haalt uren, minuten en seconden uit het systeem
+		SimpleDateFormat uur = new SimpleDateFormat("HH");
+		SimpleDateFormat minuut = new SimpleDateFormat("mm");
+		SimpleDateFormat seconde = new SimpleDateFormat("ss");	
+//		parsed de tijd in int en maakt secondes van de tijd van de huidige vraag
+	    int uurSeconden = Integer.parseInt(uur.format(resultdate))*3600;
+	    int minuutSeconden = Integer.parseInt(minuut.format(resultdate))*60;
+	    int seconden = Integer.parseInt(seconde.format(resultdate));
+	    int totaal = uurSeconden + minuutSeconden + seconden;
+//	    maakt secondes van de tijd van de vorige vraag
+	    int uSeconden = ur*3600;
+	    int mSeconden = min*60;
+	    int totaal2 = uSeconden+mSeconden+sec;
+	    return totaal-totaal2;
+	}	
 }
